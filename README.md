11b41fe612ac264f2dfe4a2fb2a63a478e18c592
Anton Lebedev <mail@lebster.me>
2019-12-08 02:14:20 +0300

    Use redis as queue.

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 Build on top of [youtube-dl](https://github.com/ytdl-org/youtube-dl).
 
 Download videos from youtube.com or other video platforms to Dropbox.
+
+### Start services
+
+Update `.env` file:
+
+```
+REDIS_URL=redis://redis:6379
+```
+
+```
+docker-compose -f docker-compose.yml up --build -d
+```
+
+### Start Redis locally
+
+```
+docker-compose -f docker-compose.redis.yml up --build --remove-orphans
+```
+
+Update `.env` file to run application separately:
+
+```
+REDIS_URL=redis://127.0.0.1:6379
+```
+
+Connect to Redis:
+
+```
+redis-cli -h 127.0.0.1 -p 6379
+```
+
+Show all keys:
+
+```
+127.0.0.1:6379> keys *
+```
--- app/app.py
+++ app/app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
+from redis import Redis
+from rq import Queue
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__file__)
 
 app = Flask(__name__)
 
+REDIS_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379')
+redis_conn = Redis.from_url(REDIS_URL)
+q = Queue(connection=redis_conn)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/download', methods=['POST'])
+def download():
+    yt_url = request.json.get('url', '')
+    if not yt_url.strip():
+        return jsonify({"error": "Url can not be empty."}), 400
+
+    try:
+        job = q.enqueue('download', yt_url)
+        logger.debug(job)
+        return jsonify({"job_id": job.id})
+    except Exception as e:
+        logger.error("Unexpected error: ", e)
+        return jsonify({"error": f"Service unavailable: {e}"}), 500
+
+
+@app.route('/list_jobs', methods=['GET'])
+def list_jobs():
+    queued_jobs_ids = q.get_job_ids()
+    return jsonify(queued_jobs_ids)
 
 
 FLASK_HOST = os.getenv('FLASK_HOST', '127.0.0.1')
